fb7bbf41ed60d3977f105bdbe7813cc5ec6cd534
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-07-10 21:21:38 +0500

    add get update and delete notes api

--- controlers/notesControler.js
+++ controlers/notesControler.js
@@ -1,18 +1,16 @@
 const { Notes } = require("../models");
 const appError = require("../utils/appError");
-exports.addNotes = async (req, res) => {
+exports.addNotes = async (req, res, next) => {
   try {
     const { heading, subHeading, userId } = req.body;
     if (!userId) {
-      return next(
-        new appError("UserId is required", 401)
-      );
+      return next(new appError("UserId is required", 401));
     }
     const newNotes = await Notes.create({
       userId,
       heading,
-      subHeading
-    })
+      subHeading,
+    });
     res.status(201).json({
       status: "success",
       data: {
@@ -26,4 +24,81 @@ exports.addNotes = async (req, res) => {
       message: err,
     });
   }
-}
+};
+
+exports.singleNotes = async (req, res, next) => {
+  try {
+    const { notesId } = req.query;
+    if (!notesId) {
+      return next(new appError("NotesId is required", 401));
+    }
+    const notes = await Notes.findOne({ where: { id: notesId } });
+    res.status(201).json({
+      status: "success",
+      data: {
+        notes,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({
+      status: "Fail",
+      message: err,
+    });
+  }
+};
+
+exports.updateNotes = async (req, res, next) => {
+  try {
+    const { notesId } = req.query;
+    if (!notesId) {
+      return next(new appError("NotesId is required", 401));
+    }
+    const notes = await Notes.findByPk(notesId);
+    if (!notes) {
+      return next(new appError("Notes does not exist", 401));
+    }
+    req.body.status = "completed";
+    const updatedNotes = await notes.update(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        updatedNotes,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({
+      status: "Fail",
+      message: err,
+    });
+  }
+};
+
+exports.deleteNotes = async (req, res, next) => {
+  try {
+    const { notesId } = req.query;
+    if (!notesId) {
+      return next(new appError("NotesId is required", 401));
+    }
+    const notes = await Notes.findByPk(notesId);
+    if (!notes) {
+      return next(new appError("Notes does not exist", 401));
+    }
+    const deletedNotes = await notes.destroy();
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        deletedNotes,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({
+      status: "Fail",
+      message: err,
+    });
+  }
+};
--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -2,6 +2,9 @@ const notesControler = require("../controlers/notesControler");
 const { protect } = require("../midlewares/protection")
 const express = require("express");
 const notesRouter = express.Router();
-const { addNotes } = notesControler
+const { addNotes, singleNotes, updateNotes, deleteNotes } = notesControler
 notesRouter.route("/addNotes").post(protect, addNotes);
+notesRouter.route("/singleNotes").get(protect, singleNotes);
+notesRouter.route("/updateNotes").patch(protect, updateNotes);
+notesRouter.route("/deleteNotes").delete(protect, deleteNotes);
 module.exports = notesRouter
